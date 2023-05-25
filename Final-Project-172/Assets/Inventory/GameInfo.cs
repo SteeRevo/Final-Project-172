7648ea5167dfd600971ecc54421721a7d42a0a2f
kofi1123 <topratedgames@gmail.com>
2023-05-25 14:37:15 -0700

    Fixing compilation error from last commit

--- Final-Project-172/Assets/Inventory/GameInfo.cs
+++ Final-Project-172/Assets/Inventory/GameInfo.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameInfo : MonoBehaviour
 {
     void Awake() {
-        DontDestoryOnLoad(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
     }
     // Start is called before the first frame update
     void Start()
